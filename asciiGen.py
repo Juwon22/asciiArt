40b31ca93894d30d388df6f806d13ebc5e7a7bd1
juwonA <adeolo1@mcmaster.ca>
2020-11-05 19:53:11 -0600

    python script to convert images to ascii representation

---
+++ asciiGen.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import math
+
+im = Image.open("rainbow.jpg")
+
+
+ScaleFactor = 0.6
+width, height = im.size
+im = im.resize((int(ScaleFactor * width), int(ScaleFactor * height)), Image.NEAREST)
+width, height = im.size
+pix = im.load()
+
+# ascii character density gradient
+asciiMap = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+charArr = list(asciiMap)
+length = len(charArr)
+interval = length / 256
+
+def getChar(inputInt):
+    return charArr[math.floor(inputInt * interval)]
+
+outputFile = open("rainbow.txt", "w")
+
+for i in range(height):
+    for j in range(width):
+        r, g, b = pix[j,i]
+        h = int(r/3 + g/3 + b/3)
+        pix[j, i] = (h, h, h)
+        outputFile.write(getChar(h))
+
+    outputFile.write("\n")
+
+# im.save("output.jpg")
+
+
